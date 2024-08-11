41b3047e503d022d0cc6e706ebfe2ada1ff28b23
Reese Hodge <reese.hodge@IntelliTect.com>
2024-08-11 08:50:27 -0700

    Renamed the Text property to Description

--- src/BedrockImageWeb/Pages/Home.razor.cs
+++ src/BedrockImageWeb/Pages/Home.razor.cs
@@ -4,14 +4,15 @@ namespace BedrockImageWeb.Pages;
 
 public partial class Home
 {
-    private string? Text { get; set; }
+    private string? Description { get; set; }
 
     private string? Image { get; set; } = "./icon-192.png";
 
     private async Task GenerateImage()
     {
+        // Example API gateway call https://{api-id}.execute-api.region.amazonaws.com/DEV/image
         var httpClient = new HttpClient();
-        var response = await httpClient.PostAsJsonAsync("api/image", new { Text });
+        var response = await httpClient.PostAsJsonAsync("api/image", new { Description });
         Image = await response.Content.ReadAsStringAsync();
     }
 }
